7ee4ece1b8c23f800c46bf25c1e0dbb8fdc2e24b
berrondo <claudio.berrondo@gmail.com>
2020-10-10 21:26:48 +0000

    refatora estrategia ainda sem jogador proprietario

--- simob.py
+++ simob.py
@@ -1,8 +1,8 @@
 # Mantenha o seu cursor aqui quando não estiver pilotando: 
 
 import random
-import pytest
-pytest.main([__file__, '-v', '-p', 'no:warnings'])
+# import pytest
+# pytest.main([__file__, '-v', '-p', 'no:warnings'])
 
 class Propriedade:
     def __init__(self, preco=300, aluguel=100, proprietario=False):
@@ -10,124 +10,41 @@ class Propriedade:
         self.aluguel = aluguel
         self.proprietario = proprietario
 
+def impulsivo(jogador, propriedade):
+    return jogador.saldo >= propriedade.preco and not propriedade.proprietario
+
+def exigente(jogador, propriedade):
+    return impulsivo(jogador, propriedade) and propriedade.aluguel > 50
+
+def cauteloso(jogador, propriedade):
+    return impulsivo(jogador, propriedade) and (jogador.saldo - propriedade.preco) >= 80
+
+def aleatorio(jogador, propriedade):
+    return impulsivo(jogador, propriedade) and random.choice([True, False])
 
 class Jogador:
     def __init__(self, estrategia, saldo=300):
         self.saldo = saldo
         self.estrategia = estrategia
 
-    def paga(self, valor):
-        self.saldo -= valor
+    def paga(self, propriedade):
+        if self.estrategia(self, propriedade):
+            self.saldo -= propriedade.preco
+        else:
+            self.saldo -= propriedade.aluguel
 
-    def __bool__(self):
-        return self.saldo >= 0
+#     def paga(self, valor):
+#         self.saldo -= valor
 
-def test_saldo_jogador():
-    jogador = Jogador(estrategia='impulsivo')
-    jogador.paga(100)
-    assert jogador
-    assert jogador.saldo == 200
+#     def __bool__(self):
+#         return self.saldo >= 0
 
-def test_jogador_eh_falso_quanto_tem_saldo_negativo():
-    jogador = Jogador(estrategia='impulsivo', saldo=-100)
-    assert not jogador
+# def test_saldo_jogador():
+#     jogador = Jogador(estrategia='impulsivo')
+#     jogador.paga(100)
+#     assert jogador
+#     assert jogador.saldo == 200
 
-
-def devo_comprar(estrategia, saldo, propriedade):
-    preco = propriedade.preco
-    aluguel = propriedade.aluguel
-    
-    if propriedade.proprietario:
-        return False
-    if saldo < preco:
-        return False
-    if estrategia == 'impulsivo':
-        return True
-    if estrategia == 'cauteloso':
-        return (saldo - preco) >= 80
-    if estrategia == 'aleatorio':
-        return random.choice([True, False])
-    if estrategia == 'exigente':
-        return aluguel > 50
-
-
-def test_comprar_com_proprietario():
-    assert devo_comprar('exigente', saldo=300, propriedade=Propriedade(100, 51, True)) == False
-
-def test_exigente_sem_saldo():
-    assert devo_comprar('exigente', saldo=0, propriedade=Propriedade(100, 51)) == False
-
-def test_exigente_com_saldo_mas_aluguel_inferior_a_50():
-    assert devo_comprar('exigente', saldo=300, propriedade=Propriedade(100, 40)) == False
-
-def test_exigente_com_saldo():
-    assert devo_comprar('exigente', saldo=300, propriedade=Propriedade(100, 51)) == True
-
-def test_aleatorio_sem_saldo_true():
-    random.seed(42)
-    assert devo_comprar('aleatorio', saldo=0, propriedade=Propriedade(100)) == False
-
-def test_aleatorio_com_saldo_false():
-    random.seed(0)
-    assert devo_comprar('aleatorio', saldo=300, propriedade=Propriedade(100)) == False
-
-def test_aleatorio_com_saldo_true():
-    random.seed(42)
-    assert devo_comprar('aleatorio', saldo=300, propriedade=Propriedade(100)) == True
-
-def test_cauteloso_com_saldo_restante_igual_a_80():
-    assert devo_comprar(estrategia='cauteloso', saldo=300, propriedade=Propriedade(220)) == True
-
-def test_cauteloso_com_saldo_restante_maior_que_80():
-    assert devo_comprar(estrategia='cauteloso', saldo=300, propriedade=Propriedade(210)) == True
-
-def test_cauteloso_com_saldo_restante_menor_que_80():
-    assert devo_comprar(estrategia='cauteloso', saldo=300, propriedade=Propriedade(230)) == False
-
-def test_impulsivo_com_saldo():
-    assert devo_comprar(estrategia='impulsivo', saldo=300, propriedade=Propriedade(150)) == True
-
-def test_impulsivo_com_saldo_igual_preco():
-    assert devo_comprar(estrategia='impulsivo', saldo=150, propriedade=Propriedade(150)) == True
-
-def test_impulsivo_sem_saldo():
-    assert devo_comprar(estrategia='impulsivo', saldo=0, propriedade=Propriedade(150)) == False    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# @pytest.mark.parametrize("params, expected", (
-#     ((), True),
-# ))
-# def test_name(params, expected):
-#     assert True
+# def test_jogador_eh_falso_quanto_tem_saldo_negativo():
+#     jogador = Jogador(estrategia='impulsivo', saldo=-100)
+#     assert not jogador
---
+++ test_simob.py
@@ -0,0 +1,85 @@
+from simob import *
+
+def test_comprar_com_proprietario():
+    jogador = Jogador(exigente)
+    propriedade = Propriedade(100, 51, True)
+    jogador.paga(propriedade)
+    assert jogador.saldo == 249
+
+def test_exigente_sem_saldo():
+    jogador = Jogador(exigente)
+    propriedade = Propriedade(400, 51)
+    jogador.paga(propriedade)
+    assert jogador.saldo == 249
+
+def test_exigente_com_saldo_mas_aluguel_inferior_a_50():
+    jogador = Jogador(exigente)
+    propriedade = Propriedade(100, 40)
+    jogador.paga(propriedade)
+    assert jogador.saldo == 260
+
+def test_exigente_com_saldo():
+    jogador = Jogador(exigente)
+    propriedade = Propriedade(100, 51)
+    jogador.paga(propriedade)
+    assert jogador.saldo == 200
+
+def test_aleatorio_sem_saldo_true():
+    random.seed(42)
+
+    jogador = Jogador(aleatorio)
+    propriedade = Propriedade(400, 51)
+    jogador.paga(propriedade)
+    assert jogador.saldo == 249
+
+def test_aleatorio_com_saldo_false():
+    random.seed(0)
+
+    jogador = Jogador(aleatorio)
+    propriedade = Propriedade(100, 51)
+    jogador.paga(propriedade)
+    assert jogador.saldo == 249
+
+def test_aleatorio_com_saldo_true():
+    random.seed(42)
+
+    jogador = Jogador(aleatorio)
+    propriedade = Propriedade(100, 51)
+    jogador.paga(propriedade)
+    assert jogador.saldo == 200
+
+def test_cauteloso_com_saldo_restante_igual_a_80():
+    jogador = Jogador(cauteloso)
+    propriedade = Propriedade(220, 51)
+    jogador.paga(propriedade)
+    assert jogador.saldo == 80
+
+def test_cauteloso_com_saldo_restante_maior_que_80():
+    jogador = Jogador(cauteloso)
+    propriedade = Propriedade(210, 51)
+    jogador.paga(propriedade)
+    assert jogador.saldo == 90
+
+def test_cauteloso_com_saldo_restante_menor_que_80():
+    jogador = Jogador(cauteloso)
+    propriedade = Propriedade(230, 51)
+    jogador.paga(propriedade)
+    assert jogador.saldo == 249
+
+def test_impulsivo_com_saldo():
+    jogador = Jogador(impulsivo)
+    propriedade = Propriedade(150, 51)
+    jogador.paga(propriedade)
+    assert jogador.saldo == 150
+
+def test_impulsivo_com_saldo_igual_preco():
+    jogador = Jogador(impulsivo)
+    propriedade = Propriedade(300, 51)
+    jogador.paga(propriedade)
+    assert jogador.saldo == 0
+
+def test_impulsivo_sem_saldo():
+    jogador = Jogador(impulsivo)
+    propriedade = Propriedade(301, 51)
+    jogador.paga(propriedade)
+    assert jogador.saldo == 249
