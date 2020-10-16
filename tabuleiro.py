e1c6993288d017e32d1d5618c317581536ba0d1f
berrondo <claudio.berrondo@gmail.com>
2020-10-16 03:59:44 +0000

    tabuleiro: compra da propriedade e bonus de 100

--- tabuleiro.py
+++ tabuleiro.py
@@ -6,7 +6,8 @@ def dado():
 
 
 class Tabuleiro:
-    def __init__(self, propriedades, jogadores):        
+    def __init__(self, propriedades, jogadores):
+        self.propriedades = propriedades     
         self.jogadores = {}
         for jogador in jogadores:
             self.jogadores[jogador] = 0
@@ -14,9 +15,18 @@ class Tabuleiro:
     def movimenta(self, jogador, passos):
         posicao_atual = self.jogadores[jogador]
         nova_posicao = posicao_atual + passos
+
         if nova_posicao > 20:
             nova_posicao -= 20
+            jogador.saldo += 100 
+
         self.jogadores[jogador] = nova_posicao
+
+        if self.propriedades:
+            propriedade = self.propriedades[nova_posicao]
+
+            jogador.paga(propriedade)
+
     
     def posicao(self, jogador):
         return self.jogadores[jogador]
